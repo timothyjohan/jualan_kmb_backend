da579733829a34ee532e5c8162d945d8fe4f0602
lthreen <timothyjohan11@gmail.com>
2023-11-14 16:09:39 +0700

    Get all htrans and post new htrans

---
+++ models/Htrans.js
@@ -0,0 +1,40 @@
+const { getDB } = require("../config/sequelize");
+const sequelize = getDB();
+const { Model, DataTypes } = require("sequelize");
+
+class Htrans extends Model { }
+Htrans.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        nama: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        menu: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        jumlah: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+        },
+        subtotal: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        modelName: "Htrans",
+        tableName: "htrans",
+    }
+),
+    module.exports = Htrans
--- routes/htrans.js
+++ routes/htrans.js
@@ -2,11 +2,31 @@ const { default: axios } = require('axios');
 const { query } = require('express');
 const express = require('express');
 const router = express.Router();
-// const User = require('../models/User');
+const Htrans = require('../models/Htrans');
 
 router.get('/get', async (req, res) => {
+    let htrans = await Htrans.findAll()
     return res.status(200).send({
-        message:"Masuk get"
+        htrans
+    })
+})
+router.post('/:nama/:menu/:jumlah/:subtotal', async (req, res) => {
+    const { nama, menu, jumlah, subtotal } = req.params
+    let result
+    let tempId = await Htrans.count() + 1
+    try {
+        result = await Htrans.create({
+            id: tempId,
+            nama:nama,
+            menu:menu,
+            jumlah:jumlah,
+            subtotal:subtotal
+        })
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+    return res.status(201).send({
+        result
     })
 })
 
