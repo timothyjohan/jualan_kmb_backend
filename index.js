dfa130a838bb5b30f39e32928f145b029c94db6b
lthreen <timothyjohan11@gmail.com>
2024-10-29 21:02:22 +0700

    env example + setup

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors'); // Add this line to import the cors middleware
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
 const app = express();
-const port = 3069;
 const htrans = require('./routes/htrans');
 
 app.use(cors()); // Enable CORS for all routes
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT, async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.klxoze2.mongodb.net/${process.env.DB_NAME}`
+        );
+        console.log("hehehhehee");
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
